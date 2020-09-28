81f16a6fcb727fbf41a9a7acca4669f7a4dbdc7a
黄伟 <554610288@qq.com>
2020-09-29 00:38:22 +0800

    vue-cli start

---
+++ src/views/NewHome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+        <h1>This is New Home</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NewHome"
+    }
+</script>
+
+<style scoped>
+
+</style>
